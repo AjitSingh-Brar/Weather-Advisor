2e94b3d7f4381b2821cd4be6a2bc634e11893c04
NS918326 <Navrajgaltee@outlook.com>
2024-06-27 10:49:17 -0400

    UPDT: Code refactored

--- utils/checkWeather.ts
+++ utils/checkWeather.ts
@@ -1,22 +1,29 @@
+import {
+  storeError,
+  storeInfo,
+} from "@/lib/store/features/weather/weatherSlice";
 
-import { storeError, storeInfo } from "@/lib/store/features/weather/weatherSlice";
+export const getCityWeatherDetails = async (
+  cityName: string,
+  countryCode: string,
+  API_KEY: string | undefined,
+  dispatch: any
+) => {
+  const URL = "https://api.openweathermap.org/data/2.5/weather";
+  const FULL_URL = `${URL}?q=${cityName},${countryCode}&appid=${API_KEY}`;
 
-export const getCityWeatherDetails = async (cityName: string, countryCode: string, API_KEY: string | undefined, dispatch: any) => {
-    const URL = "https://api.openweathermap.org/data/2.5/weather";
-    const FULL_URL = `${URL}?q=${cityName},${countryCode}&appid=${API_KEY}`;
-
-    try {
-      const response = await fetch(FULL_URL);
-      const data = await response.json();
-      const dataInfo = {
-        city: data.name,
-        countryCode: data.sys.country,
-        temperature: data.main.temp,
-        humidity: data.main.humidity,
-        windSpeed: data.wind.speed,
-      };
-      dispatch(storeInfo(dataInfo));
-    } catch (error) {
-      dispatch(storeError(error));
-    }
-  };
+  try {
+    const response = await fetch(FULL_URL);
+    const data = await response.json();
+    const dataInfo = {
+      city: data.name,
+      countryCode: data.sys.country,
+      temperature: data.main.temp,
+      humidity: data.main.humidity,
+      windSpeed: data.wind.speed,
+    };
+    dispatch(storeInfo(dataInfo));
+  } catch (error) {
+    dispatch(storeError(error));
+  }
+};
